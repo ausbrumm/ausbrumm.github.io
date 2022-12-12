8ae5c994d8ec9a7a89663d5c4e0755d4c62c04ba
austinbrummett <austin.brummett@outlook.com>
2022-12-12 11:37:19 -0500

    Updated the navigation

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+open.addEventListener("click", () => container.classList.add("show-nav"));
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener("click", () => container.classList.remove("show-nav"));
 
 // add event listeners to the nav icons
-document.querySelectorAll('.fas').forEach(e =>{
-    e.addEventListener('click', (e)=>{
-       if(e.target.id != ''){     
-            window.location.href = e.target.id + '.html'
-       }
-    })
-})
+document.querySelectorAll(".fas").forEach((e) => {
+  e.addEventListener("click", (e) => {
+    if (e.target.id != "") {
+      if (e.target.id != "index") {
+        console.log(e.target.id);
+        window.location.href = "/html/" + e.target.id + ".html";
+      } else window.location.href = "../" + e.target.id + ".html";
+    }
+  });
+});
